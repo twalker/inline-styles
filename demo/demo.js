3124a4b8fa213032eab2fa4bfda0cc7f19ad8eb2
tim walker <tim.twalker@gmail.com>
2014-08-03 20:55:36 -0700

    working demo

--- demo/demo.js
+++ demo/demo.js
@@ -3,21 +3,32 @@
  */
 require.config({
   paths: {
+    text: '../bower_components/requirejs-text/text',
     style2style: '../style2style'
   }
 });
 
-require(['style2style'], function(style2style){
+require(['style2style', 'text!../test/fixture.html'], function(style2style, fixture){
 
+  var iframe = document.getElementById('iframe');
   var form = document.querySelector('form');
+  var src = form.querySelector('[name="src"]');
+  src.value = fixture;
+
+  src.addEventListener('input', function(e){
+    console.log('input', iframe);
+    //iframe.src = 'data:text/html,' + e.currentTarget.value;
+    iframe.srcdoc = e.currentTarget.value;
+  })
+
+  iframe.srcdoc = fixture;
+
   var onSubmit = function(e){
     e.preventDefault();
-    console.log('click');
-    var iframe = document.createElement('iframe');
-    iframe.src = 'data:text/html,' + form.querySelector('[name="src"]').value;
-    document.body.appendChild(iframe);
-    style2style(iframe.ownerDocument);
-    form.querySelector('[name="dest"]').value = iframe.body.innerHTML;
+    var doc = iframe.contentDocument;
+
+    style2style(doc, JSON.parse(form.querySelector('[name="options"]').value));
+    form.querySelector('[name="dest"]').value = doc.head.outerHTML + '\n' + doc.body.outerHTML;
 
   };
   form.addEventListener('submit', onSubmit)
